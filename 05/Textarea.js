e5efa1dfcea8281c1fe5ff9655daee61bbff1660
Daria Seńko <twoj@email>
2025-12-22 17:43:54 +0100

    projekt

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
+  textareaRef = React.createRef();
+
+  getSnapshotBeforeUpdate(prevProps) {
+    if (prevProps.content !== this.props.content) {
+      const textarea = this.textareaRef.current;
+
+      if (textarea.offsetHeight < textarea.scrollHeight) {
+        return { resize: true };
+      }
     }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot?.resize) {
+      const textarea = this.textareaRef.current;
+
+      const newHeight = Math.min(textarea.scrollHeight, 100);
+      textarea.style.height = `${newHeight}px`;
+    }
+  }
+
+  handleChange = (e) => {
+    this.props.onChange(e.target.value);
+  };
+
+  render() {
+    return (
+      <textarea
+        ref={this.textareaRef}
+        value={this.props.content}
+        onChange={this.handleChange}
+        style={{
+          resize: 'none',
+          overflow: 'hidden',
+          height: '40px',
+        }}
+      />
+    );
+  }
 }
 
 export default Textarea;
